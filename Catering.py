06dc205aba0c1e002e8f9f2278578082c498a4c9
JaredJRoss <jaredrossj@gmail.com>
2017-03-20 16:44:37 -0400

    added basic data manipulation on inquiry file

--- Catering.py
+++ Catering.py
@@ -12,4 +12,11 @@ location = str(filename)
 
 # convert the contents of that file in a df
 df = pd.read_csv(location)
+df.drop(df.index[:5], inplace = True)
+df = df.reset_index(drop=True)
+df.columns = df.iloc[0]
+df.drop(df.index[0], inplace = True)
+df = df.reset_index(drop=True)
 print(df)
+df.drop(df.index[2],inplace = True)
+df.to_csv('inq.csv')
